b133eccde825859270e025b307e6876324fb9d00
Khandaker Ziaur Rahman Rehan <zrrehan@outlook.com>
2025-03-02 05:05:02 +0600

    Developed Header

---
+++ script.js
@@ -0,0 +1,5 @@
+let colorBtn = document.querySelector("#colorBTN");
+colorBtn.addEventListener("click", function() {
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "red";
+});
